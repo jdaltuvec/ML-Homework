3d7aa24f6ca595bd92f833874ffca4ee69f82dc8
David A <josedavac@gmail.com>
2020-05-09 17:49:12 -0500

    Added some math to pipeline

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,9 @@
 def prep_data(df):
 
-    df = df.assign(hw=df["Height"] * df["Width"])
+    df = df.assign(hw = df["Height"] * df["Width"] )
+    df = df.assign(all_l = (df["Length1"] * df["Length2"] * df["Length3"])/3 )
 
-    X = df[["Height", "Width", "Length1", "Length2", "Length3", "hw"]].values
+    X = df[["hw", "all_l"]].values
     y = df["Weight"].values
 
     return X, y
